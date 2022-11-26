265f48feeb10c05e20a6d94e1ea4ac5db38151ee
Méline Dang <84090602+MelineDang@users.noreply.github.com>
2022-11-26 22:31:33 +0100

    changement de note depuis page détaillée élève + readme + esthétique couleurs

--- README.md
+++ README.md
@@ -3,19 +3,22 @@ E5 DSIA
 DANG Méline et VAN ROOIJ Théo
 
 
+# Comment lancer le projet ? 
 Pour lancer notre projet il vous suffit de :
 - lancer votre Docker ;
 - aller dans votre Terminal et vous placer dans le dossier de notre projet ;
 - lancer un "docker-compose up -d" depuis le Terminal ;
-- vous rendre sur un navigateur et aller sur la page "http://localhost:8050/".
+- vous rendre sur un navigateur et rendez-vous sur la page "http://localhost:8050/".
 
 
+# Pourquoi ce projet, et comment fonctionne-t-il ?
 Notre projet vise à réaliser une plateforme de vie scolaire pour les écoles.
 Il est possible d'accéder à quatre pages par notre barre de navigation :
 - la page d'accueil, là où nous expliquons d'accueil brièvement le but de notre projet ;
 - la page élèves, c'est ici que se regroupe tous les élèves comtenus dans notre database ;
 - la page professeurs, c'est ici que se regroupe tous les professeurs de notre database ;
 - la page matières/unités, nous y retrouvons toutes les matières enseignées par les professeurs, et suivies par les élèves de notre database.
+
 
 ## Page Elèves
 La page élèves regroupe tous les élèves de la database en plusieurs colonnes détaillant : 
@@ -24,18 +27,18 @@ La page élèves regroupe tous les élèves de la database en plusieurs colonnes
 - dates de naissance ;
 - niveaux scolaires ;
 - classes ;
-- matières à lesquelles ils sont inscrits.
-Nous pouvons ajouter directement des élèves en clickant sur le bouton "Ajouter un élève" en haut à droite de notre page. Depuis la page de création, nous pouvons rentrer les détails mentionnés au-dessus, et sélectionner les matières à lesquelles l'élève est inscrit (il faut au préalable ajouter des matières dans la page Matières/Unités).
-Pour inscrire les élèves à plusieurs matières, il faut cliquer sur CTRL et sélectionner les matières voulues, ou bien SHIFT pour en sélectionner plusieurs de suite.
-Nous pouvons gérer les élèves à partir de cette page, modifier leurs informations si besoin, ou bien même supprimer des élèves depuis la colonne "Actions" et des boutons "Modifier" et "Supprimer".
+- matières auxquelles ils sont inscrits.
+Nous pouvons ajouter directement des élèves en cliquant sur le bouton "Ajouter un élève" en haut à droite de notre page. Depuis la page de création, nous pouvons rentrer les détails mentionnés au-dessus, et sélectionner les matières auxquelles l'élève est inscrit (il faut au préalable ajouter des matières dans la page Matières/Unités).
+Pour inscrire les élèves à plusieurs matières, il faut maintenir la touche CTRL et sélectionner les matières voulues, ou bien SHIFT pour en sélectionner plusieurs de suite.
+Nous pouvons gérer les élèves à partir de cette page, c'est-à-dire : voir la page détaillée des élèves, modifier leurs informations si besoin, ou bien même supprimer des élèves depuis la colonne "Actions", respectivement par les boutons "Détails", "Modifier" et "Supprimer".
 
 ### Page d'un élève spécifique
-Il est possible de clicker sur le nom ou prénom d'un élève en particulier (directemenet depuis la table), pour accéder à sa page élève spécifique dans laquelle nous pouvons voir :
+Il est possible de cliquer sur le bouton "Détails" de la Page Elèves, ou bien sur le nom ou prénom d'un élève en particulier (directemenet depuis la table de la Page Elèves), pour accéder à sa page élève spécifique dans laquelle nous pouvons voir :
 - nom et prénom de l'élève ;
 - niveau scolaire et classe de l'élève ;
 - date de naissance de l'élève ;
-- tous les cours où est inscrit l'élève, il est possible de le désinscrire d'un cours depuis cette page.
-Nous pouvons aussi modifier directement l'élève en clickant sur le bouton "Modifier l'élève" en haut à droite de notre page.
+- tous les cours où est inscrit l'élève, il est possible de modifier la note de l'élève de chaque matière en particulier, d'accéder à la page détaillée de la matière, ou de le désinscrire d'une unité depuis cette page en cliquant respectivement sur les boutons symbole de Edit de la note (le petit dessin avec une feuille et un crayon), "Détails" et "Désinscrire".
+Nous pouvons aussi modifier directement les informations de l'élève en cliquant sur le bouton "Modifier l'élève" en haut à droite de notre page.
 
 
 ## Page Professeurs
@@ -44,30 +47,29 @@ La page élèves regroupe tous les professeurs de la database en plusieurs colon
 - prénoms ;
 - dates de naissance ;
 - matières qu'ils enseignent.
-Nous pouvons ajouter directement des professeurs en clickant sur le bouton "Ajouter un professeur" en haut à droite de notre page. Depuis la page de création, nous pouvons rentrer les détails mentionnés au-dessus, et sélectionner les matières qu'enseigne le professeur (il faut au préalable ajouter des matières dans la page Matières/Unités).
-Pour associer les professeurs à plusieurs matières, il faut cliquer sur CTRL et sélectionner les matières voulues, ou bien SHIFT pour en sélectionner plusieurs de suite.
-Nous pouvons gérer les élèves à partir de cette page, modifier leurs informations si besoin, ou bien même supprimer des professeurs depuis la colonne "Actions" et des boutons "Modifier" et "Supprimer".
+Nous pouvons ajouter directement des professeurs en cliquant sur le bouton "Ajouter un professeur" en haut à droite de notre page. Depuis la page de création, nous pouvons rentrer les détails mentionnés au-dessus, et sélectionner les matières qu'enseigne le professeur (il faut au préalable ajouter des matières dans la page Matières/Unités).
+Pour associer les professeurs à plusieurs matières, il faut maintenir la touche CTRL et sélectionner les matières voulues, ou bien SHIFT pour en sélectionner plusieurs de suite.
+Nous pouvons gérer les professeurs à partir de cette page, c'est-à-dire : voir la page détaillée des professeurs, modifier leurs informations si besoin, ou bien même supprimer des professeurs depuis la colonne "Actions", respectivement par les boutons "Détails", "Modifier" et "Supprimer".
 
 ### Page d'un professeur spécifique
-Il est possible de clicker sur le nom ou prénom d'un professeur en particulier (directemenet depuis la table), pour accéder à sa page professeur spécifique dans laquelle nous pouvons voir :
+Il est possible de cliquer sur le bouton "Détails" de la Page Professeurs, ou bien sur le nom ou prénom d'un professeur en particulier (directemenet depuis la table de la Page Professeurs), pour accéder à sa page professeur spécifique dans laquelle nous pouvons voir :
 - nom et prénom du professeur ;
 - niveau scolaire et classe du professeur;
 - date de naissance du professeur ;
-- tous les cours qu'enseigne le/la professeur, il est possible de le désassocier d'un cours depuis cette page.
-Nous pouvons aussi modifier directement le/la professeur en clickant sur le bouton "Modifier le/la professeur" en haut à droite de notre page.
+- tous les cours qu'enseigne le/la professeur, il est possible d'accéder à la page détaillée de la matière, ou de le désassocier d'une unité depuis cette page en cliquant respectivement sur les boutons "Détails" et "Désinscrire".
+Nous pouvons aussi modifier directement le/la professeur en cliquant sur le bouton "Modifier le/la professeur" en haut à droite de notre page.
 
 
 ## Page Matières/Unités
-La page Latières/Unités regroupe toutes les matières de la database en plusieurs colonnes détaillant : 
+La page Matières/Unités regroupe toutes les matières de la database en plusieurs colonnes détaillant : 
 - code de la matière/unité ;
 - nom de la matière/unité.
-Nous pouvons ajouter directement des matières en clickant sur le bouton "Ajouter une matière/unité" en haut à gauche de notre page. Depuis la page de création, nous pouvons rentrer les détails mentionnés au-dessus.
-Nous pouvons gérer les matières/unités à partir de cette page, modifier leurs informations si besoin, ou bien même supprimer des matières depuis la colonne "Actions" et des boutons "Modifier" et "Supprimer".
+Nous pouvons ajouter directement des matières en cliquant sur le bouton "Ajouter une matière/unité" en haut à gauche de notre page. Depuis la page de création, nous pouvons rentrer les détails mentionnés au-dessus.
+Nous pouvons gérer les matières à partir de cette page, c'est-à-dire : voir la page détaillée des matières, modifier leurs informations si besoin, ou bien même supprimer des matières depuis la colonne "Actions", respectivement par les boutons "Détails", "Modifier" et "Supprimer".
 
-<!--
-En cours de code
  ### Page d'une matière/unité spécifique
-Il est possible de clicker sur le code ou le nom d'une matière/unité en particulier (directemenet depuis la table), pour accéder à sa page matière/unité spécifique dans laquelle nous pouvons voir :
+Il est possible de cliquer sur le bouton "Détails" de la Page Matières/Unités, ou bien sur le nom ou prénom d'une matière/unité en particulier (directement depuis la table de la Page Matières/Unités), pour accéder à sa page unité spécifique dans laquelle nous pouvons voir :
 - code et nom de l'unité ;
-- tous les élèves inscrits et professeurs associés au cours.
-Nous pouvons aussi modifier directement la matière/unité en clickant sur le bouton "Modifier l'unité" en haut à droite de notre page. -->
+- tous les professeurs associés au cours, il est possible d'accéder à la page détaillée du professeur ou bien de le désassocier du cours en utilisant respectivement les boutons "Détails" et "Désinscrire" ;
+- tous les élèves inscrits au cours ainsi que leur note générale dans cette unité, il est possible de modifier la note, de voir la page détaillée de l'élève ou de le désinscrire du cours respectivement par les boutons symbole de Edit de la note (le petit dessin avec une feuille et un crayon), "Détails" et "Désinscrire".
+Nous pouvons aussi modifier directement la matière/unité en cliquant sur le bouton "Modifier l'unité" en haut à droite de notre page.
--- front/app/main.py
+++ front/app/main.py
@@ -12,14 +12,14 @@ def root():
     return render_template("accueil.html",text=requests.get(app.config['API_URL']).text)
 
 
+
 ## Students 
 
 @app.route('/students')
 def students():
     response  = requests.get(app.config['API_URL']+"/students")
-    # return response.content
-
     return render_template("students.html",students=response.json())
+
 
 @app.route('/student/<studentID>')
 def studentDetail(studentID):
@@ -29,14 +29,13 @@ def studentDetail(studentID):
 
 @app.route('/add-student')
 def addStudent():
-
     form = studentForm()
     all_subjects  = requests.get(app.config['API_URL']+"/subjects")
     return render_template("add-student.html",form=form, all_subjects=all_subjects.json())
 
+
 @app.route('/add-student-api', methods= ['POST','GET'])
 def addStudentApi():
-
     formData = request.form.to_dict(flat=False)
     new_student = dict()
     for var,value in formData.items() : 
@@ -49,11 +48,10 @@ def addStudentApi():
     response  = requests.post(app.config['API_URL']+"/students",json=new_student)
     return redirect("/students")
 
+
 @app.route('/student/remove-subject/<studentID>/<subjectID>', methods= ['POST','GET'])
 def removeStudentSubject(studentID,subjectID):
-
     student =  requests.get(app.config['API_URL']+"/students/"+studentID).json()
-    # return student
     subjects = student.get("subjects")
     student["subjects"] = []
     for subject in subjects :
@@ -63,12 +61,11 @@ def removeStudentSubject(studentID,subjectID):
     return redirect("/student/"+studentID)
 
 
-
 @app.route('/delete-student/<studentID>', methods= ['GET'])
 def deleteStudentApi(studentID):
-
     response  = requests.delete(app.config['API_URL']+"/students/"+studentID)
     return redirect("/students")
+
 
 @app.route('/edit-student/<studentID>', methods= ['GET'])
 def updateStudent(studentID):
@@ -78,6 +75,7 @@ def updateStudent(studentID):
     form = studentForm(obj=student)
     return render_template("edit-student.html",form=form,student=student,all_subjects=all_subjects.json())
 
+
 @app.route('/edit-student-api/<studentID>', methods= ['POST','GET'])
 def editStudentApi(studentID):
     formData = request.form.to_dict(flat=False)
@@ -85,17 +83,15 @@ def editStudentApi(studentID):
     for var,value in formData.items() : 
         new_student[var] = value[0]
     new_subjects = list()
-
     subjectsForm = formData.get("subjects")
     if subjectsForm :
         for subject in  subjectsForm: 
             new_subjects.append([subject,-1])
         new_student["subjects"] = new_subjects if new_subjects else []
     new_student["id"] = studentID
-    # return new_student
     response  = requests.put(app.config['API_URL']+"/students/"+studentID,json=new_student)
-    # return response.content
     return redirect("/students")
+
 
 
 ## Teachers 
@@ -105,6 +101,7 @@ def teachers():
     response  = requests.get(app.config['API_URL']+"/teachers")
     return render_template("teachers.html",teachers=response.json())
 
+
 @app.route('/teacher/<teacherID>')
 def teacherDetail(teacherID):
     response  = requests.get(app.config['API_URL']+"/teachers/subjects/"+teacherID)
@@ -113,29 +110,25 @@ def teacherDetail(teacherID):
 
 @app.route('/add-teacher')
 def addTeacher():
-
     form = teacherForm()
     all_subjects  = requests.get(app.config['API_URL']+"/subjects")
     return render_template("add-teacher.html",form=form, all_subjects=all_subjects.json())
 
+
 @app.route('/add-teacher-api', methods= ['POST','GET'])
 def addTeacherApi():
-
     formData = request.form.to_dict(flat=False)
     new_teacher = dict()
     for var,value in formData.items() : 
         new_teacher[var] = value[0]
     new_teacher["subjects"] = formData.get("subjects")
-
     response  = requests.post(app.config['API_URL']+"/teachers",json=new_teacher)
     return redirect("/teachers")
 
 
 @app.route('/teacher/remove-subject/<teacherID>/<subjectID>', methods= ['POST','GET'])
 def removeTeacherSubject(teacherID,subjectID):
-
     teacher =  requests.get(app.config['API_URL']+"/teachers/subjects/"+teacherID).json()
-
     subjects = teacher.get("subjects")
     teacher["subjects"] = []
     for subject in subjects :
@@ -145,26 +138,23 @@ def removeTeacherSubject(teacherID,subjectID):
     return redirect("/teacher/"+teacherID)
 
 
-
 @app.route('/delete-teacher/<teacherID>', methods= ['GET'])
 def deleteTeacherApi(teacherID):
-
     response  = requests.delete(app.config['API_URL']+"/teachers/"+teacherID)
     return redirect("/teachers")
 
 
 @app.route('/edit-teacher/<teacherID>', methods= ['GET'])
 def updateTeacher(teacherID):
-
     response  = requests.get(app.config['API_URL']+"/teachers/"+teacherID)
     teacher = response.json()
     all_subjects  = requests.get(app.config['API_URL']+"/subjects")
     form = teacherForm(obj=teacher)
     return render_template("edit-teacher.html",form=form,teacher=teacher,all_subjects=all_subjects.json())
 
+
 @app.route('/edit-teacher-api/<teacherID>', methods= ['POST','GET'])
 def editTeacherApi(teacherID):
-    
     formData = request.form.to_dict(flat=False)
     new_teacher = dict()
     for var,value in formData.items() : 
@@ -173,6 +163,7 @@ def editTeacherApi(teacherID):
     new_teacher["id"] = teacherID
     response  = requests.put(app.config['API_URL']+"/teachers/"+teacherID,json=new_teacher)
     return redirect("/teachers")
+
 
 
 ## Matières/Unités
@@ -188,25 +179,27 @@ def subjectDetail(subjectID):
     students  = requests.get(app.config['API_URL']+"/subjects/students/"+subjectID)
     teachers = requests.get(app.config['API_URL']+"/subjects/teachers/"+subjectID)
     response  = requests.get(app.config['API_URL']+"/subjects/"+subjectID)
-    # return students.json()
     return render_template("subject-detail.html", subject=response.json(),students=students.json(), teachers=teachers.json())
+
 
 @app.route('/add-subject')
 def addSubject():
     form = subjectForm()
     return render_template("add-subject.html",form=form)
 
+
 @app.route('/add-subject-api', methods= ['GET'])
 def addSubjectApi():
     jsonDict = dict(request.args)
     response  = requests.post(app.config['API_URL']+"/subjects",json=jsonDict)
-
     return redirect("/subjects")
+
 
 @app.route('/delete-subject/<subjectID>', methods= ['GET'])
 def deleteSubjectApi(subjectID):
     response  = requests.delete(app.config['API_URL']+"/subjects/"+subjectID)
     return redirect("/subjects")
+
 
 @app.route('/edit-subject/<subjectID>', methods= ['GET'])
 def updateSubject(subjectID):
@@ -215,9 +208,9 @@ def updateSubject(subjectID):
     form = subjectForm(obj=subject)
     return render_template("edit-subject.html",form=form,subject=subject)
 
+
 @app.route('/edit-subject-api/<subjectID>', methods= ['GET'])
 def editSubjectApi(subjectID):
-    # return request.args
     formData = request.args.to_dict()
     new_subject = dict()
     for var,value in formData.items() : 
@@ -226,11 +219,10 @@ def editSubjectApi(subjectID):
     response  = requests.put(app.config['API_URL']+"/subjects/"+subjectID,json=new_subject)
     return redirect("/subjects")
 
+
 @app.route('/subjects/remove-teacher/<subjectID>/<teacherID>', methods= ['POST','GET'])
 def removeSubjectTeacher(subjectID,teacherID):
-
     subject =  requests.get(app.config['API_URL']+"/subjects/"+subjectID)
-    # return teacherID
     subject = subject.json()
     teachers = subject.get("teachers")
     subject["teachers"] = []
@@ -238,7 +230,6 @@ def removeSubjectTeacher(subjectID,teacherID):
         for teacher in teachers :
             if not teacher.get("id") == teacherID :
                 subject["teachers"].append(teacher["id"])
-
     students = subject.get("students")
     subject["students"] = []
     if students :
@@ -247,14 +238,12 @@ def removeSubjectTeacher(subjectID,teacherID):
             if not note : 
                 note=-1
             subject["students"].append([student["id"],note])
-    # return subject
     response  = requests.put(app.config['API_URL']+"/subjects/"+subjectID,json=subject)
-    # return response.content
     return redirect("/subject/"+subjectID)
+
 
 @app.route('/subjects/remove-student/<subjectID>/<studentID>', methods= ['POST','GET'])
 def removeSubjectStudent(subjectID,studentID):
-
     subject =  requests.get(app.config['API_URL']+"/subjects/"+subjectID)
     subject = subject.json()
     teachers = subject.get("teachers")
@@ -262,7 +251,6 @@ def removeSubjectStudent(subjectID,studentID):
     if teachers :
         for teacher in teachers :
             subject["teachers"].append(teacher["id"])
-
     students = subject.get("students")
     subject["students"] = []
     for student in students :
@@ -271,15 +259,11 @@ def removeSubjectStudent(subjectID,studentID):
             if not note : 
                 note=-1
             subject["students"].append([student["id"],note])
-    # return subject
     response  = requests.put(app.config['API_URL']+"/subjects/"+subjectID,json=subject)
-    # return response.content
     return redirect("/subject/"+subjectID)
 
 
-@app.route('/subjects/edit-note/<subjectID>/<studentID>/<noteValue>', methods= ['POST','GET'])
-def editNoteSubject(subjectID,studentID,noteValue):
-
+def changeNote(subjectID,studentID,noteValue):
     subject =  requests.get(app.config['API_URL']+"/subjects/"+subjectID)
     subject = subject.json()
     teachers = subject.get("teachers")
@@ -287,10 +271,8 @@ def editNoteSubject(subjectID,studentID,noteValue):
     if teachers :
         for teacher in teachers :
             subject["teachers"].append(teacher["id"])
-
     students = subject.get("students")
     subject["students"] = []
-
     for student in students :
         if not student.get("id") == studentID :
             note = student["note"]
@@ -300,12 +282,24 @@ def editNoteSubject(subjectID,studentID,noteValue):
         else :
             subject["students"].append([student["id"],int(noteValue)])
     response  = requests.put(app.config['API_URL']+"/subjects/"+subjectID,json=subject)
+    return response
+
+
+@app.route('/subjects/edit-note/<subjectID>/<studentID>/<noteValue>', methods= ['POST','GET'])
+def editNoteSubject(subjectID,studentID,noteValue):
+    response = changeNote(subjectID,studentID,noteValue)
     return redirect("/subject/"+subjectID)
+
+
+@app.route('/students/edit-note/<subjectID>/<studentID>/<noteValue>', methods= ['POST','GET'])
+def editNoteStudent(subjectID,studentID,noteValue):
+    response = changeNote(subjectID,studentID,noteValue)
+    return redirect("/student/"+studentID)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect("/")
-
 
 
 if __name__ == '__main__':
